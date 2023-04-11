eb416e9df0d5ed2cb7ef3d57fb1f910cdd5a9174
liuxinsheng <liuxinsheng@qq.com>
2023-04-11 15:32:19 +0800

    init

---
+++ austin-web/src/main/java/com/jason/austin/web/AustinApplication.java
@@ -0,0 +1,13 @@
+package com.jason.austin.web;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AustinApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(AustinApplication.class, args);
+        }
+}
