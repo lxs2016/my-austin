bd00915fe7dc0e311a2ec105e72790dfd508c18c
liuxinsheng <liuxinsheng@qq.com>
2023-04-18 10:44:48 +0800

    引入腾讯云短信

--- austin-web/src/main/java/com/jason/austin/web/test/Test.java
+++ austin-web/src/main/java/com/jason/austin/web/test/Test.java
@@ -8,6 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,11 +18,15 @@ import java.util.List;
  * @date 2022/6/8
  */
 
+@Slf4j
 public class Test {
+
+    private static Logger logger = org.slf4j.LoggerFactory.getLogger(Test.class);
 
     public static void main(String[] args) {
 
         try {
+            log.info("log:我真的是醉了，这都没人给三连吗？");
             LombokTest lombokTest1 = LombokTest.builder().id("1").name("能给我三连吗").build();
             LombokTest lombokTest2 = LombokTest.builder().id("2").name("对我真的很重要").build();
             List<LombokTest> lombokTests = Arrays.asList(lombokTest1, lombokTest2);
